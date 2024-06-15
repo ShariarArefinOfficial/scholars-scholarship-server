f66e24c792cedde2237d128a63d5222fcee93242
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-06-16 04:03:55 +0600

    Jwt , Middleware and users api

--- index.js
+++ index.js
@@ -34,12 +34,106 @@ async function run() {
 
     //collections
     const scholarshipCollection=Database.collection('scholarship');
+    const userCollection=Database.collection('users')
+
+    //JWt Api
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+          })
+      
+          // middlewares 
+          const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+              return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+              if (err) {
+                return res.status(401).send({ message: 'unauthorized access' })
+              }
+              req.decoded = decoded;
+              next();
+            })
+          }
+      
+          // use verify admin after verifyToken
+          const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+          }
+      
+          // users related api
+          app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+          });
+      
+          app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+      
+            if (email !== req.decoded.email) {
+              return res.status(403).send({ message: 'forbidden access' })
+            }
+      
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+              admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+          })
+      
+          app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+              return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+          });
+      
+          app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+              $set: {
+                role: 'admin'
+              }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+          })
+      
+          app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+          })
+
+
+    //User api
 
 
     //====Scholarship APi
 app.get('/scholarship', async (req, res) => {
-      const page = parseInt(req.query._page) || 1;
-      const limit = parseInt(req.query._limit) || ITEMS_PER_PAGE;
+      const page = parseInt(req.query._page);
+      const limit = parseInt(req.query._limit);
       console.log(page,limit)
       const skip = (page - 1) * limit;
       console.log("Page:", page, "Limit:", limit, "Skip:", skip);
